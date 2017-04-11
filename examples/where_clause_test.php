77fcfe0353d81a69d9f5699dc995ae9a1c8793fc
Syco <syco@ArchMac>
2017-04-11 13:18:29 +0000

    reformat every file using 2 space tabs
    Signed-off-by: Syco <syco@ArchMac>

--- examples/where_clause_test.php
+++ examples/where_clause_test.php
@@ -1,23 +1,22 @@
 <?php
+//for more documentation go to http://ajaxcrud.com/api/index.php?id=ajaxcrud_where_clause
 
-	//for more documentation go to http://ajaxcrud.com/api/index.php?id=ajaxcrud_where_clause
+session_start(); //i need in order to access session variables
 
-	session_start(); //i need in order to access session variables
+echo "<h2>Current WHERE Clauses in SESSION</h2><hr /<br /><br />\n";
 
-	echo "<h2>Current WHERE Clauses in SESSION</h2><hr /<br /><br />\n";
-
-	if (is_array($_SESSION['ajaxcrud_where_clause']) && isset($_SESSION['ajaxcrud_where_clause'])){
-		$countWhereClauses = count($_SESSION['ajaxcrud_where_clause']);
-		echo "<p>Number of WHERE Clauses in session: $countWhereClauses</p>";
-		if ($countWhereClauses > 0){
-			foreach ($_SESSION['ajaxcrud_where_clause'] as $table => $whereClause){
-				echo "&nbsp;&nbsp;Table: <b>$table</b> | WHERE Clause: <i>$whereClause</i><br />";
-			}
-		}//if any where clauses exist
-	}//if any where clauses are
-	else{
-		echo "<p>No WHERE clauses set by any ajaxCRUD table instances (yet); try going into an example and typing in a filter box; then come back and refresh this page.</p>\n";
-	}
+if (is_array($_SESSION['ajaxcrud_where_clause']) && isset($_SESSION['ajaxcrud_where_clause'])) {
+  $countWhereClauses = count($_SESSION['ajaxcrud_where_clause']);
+  echo "<p>Number of WHERE Clauses in session: $countWhereClauses</p>";
+  if ($countWhereClauses > 0) {
+    foreach ($_SESSION['ajaxcrud_where_clause'] as $table => $whereClause) {
+      echo "&nbsp;&nbsp;Table: <b>$table</b> | WHERE Clause: <i>$whereClause</i><br />";
+    }
+  }//if any where clauses exist
+}//if any where clauses are
+else {
+  echo "<p>No WHERE clauses set by any ajaxCRUD table instances (yet); try going into an example and typing in a filter box; then come back and refresh this page.</p>\n";
+}
 ?>
 <br /><hr /><br />
 
